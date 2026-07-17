9127242cadb788ef33ef1784acf65f13257ef033
João <joaopedroribeiro1204@gmail.com>
2026-07-16 23:47:10 -0300

    Correções

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+pasta_atual = os.path.dirname(__file__)
+sys.path.append(os.path.join(pasta_atual, "controlador"))
+sys.path.append(os.path.join(pasta_atual, "visao"))
+sys.path.append(os.path.join(pasta_atual, "modelo"))
+
 from tkinter import Tk
 from controller import EditorController
 
