98c64503a48aee62d127fc088064f180790aeffd
Tarcísio Fraga <tarciofragasr@gmail.com>
2026-07-16 23:33:34 -0300

    Atualização para o viewr

--- visao/view.py
+++ visao/view.py
@@ -1,24 +1,18 @@
 from tkinter import *
-from tkinter import ttk, colorchooser, messagebox
+from tkinter import ttk
 
-# Criando o editor
-
-class View:
-# Interface 
-    def __init__(self):
-        self.root = None
+class EditorView:
+    def __init__(self, root, controller):
+        self.root = root
+        self.controller = controller
+        
         self.frame = None
         self.canvas = None
         self.tipo_figura_var = None
         self.btn_cor = None
         self.btn_preenchimento = None
 
-
-    def criar_interface(self):
-
-        self.root = Tk()
-        self.root.title("Editor de Desenhos")
-
+    def criar_interface(self, cor_borda_inicial):
         self.frame = Frame(self.root)
         self.frame.pack()
 
@@ -30,9 +24,7 @@ class View:
         ).grid(column=0, row=0, sticky=W, **paddings)
 
         self.tipo_figura_var = StringVar(value="Linha")
-
-        self.tipo_figura_var.trace_add("write", self.instrucao_poligono)
-
+        self.tipo_figura_var.trace_add("write", self.controller.instrucao_poligono)
 
         ttk.OptionMenu(
             self.frame,
@@ -49,20 +41,18 @@ class View:
         self.btn_cor = Button(
             self.frame,
             text="Cor da Borda",
-            command=self.escolher_cor_borda,
-            bg=self.cor_borda,
+            command=self.controller.escolher_cor_borda,
+            bg=cor_borda_inicial,
             fg="white"
         )
-
         self.btn_cor.grid(column=0, row=1, sticky=W, **paddings)
 
         self.btn_preenchimento = Button(
             self.frame,
             text="Cor de Preenchimento",
-            command=self.escolher_cor_preenchimento,
+            command=self.controller.escolher_cor_preenchimento,
             bg="white"
         )
-
         self.btn_preenchimento.grid(column=1, row=1, sticky=W, **paddings)
 
         self.canvas = Canvas(
@@ -71,10 +61,18 @@ class View:
             height=600,
             bg="white"
         )
-
         self.canvas.grid(column=0, row=2, columnspan=2, sticky=W, **paddings)
 
-        self.canvas.bind("<Button-1>", self.iniciar_figura)
-        self.canvas.bind("<B1-Motion>", self.atualizar_figura)
-        self.canvas.bind("<ButtonRelease-1>", self.finalizar_figura)
-        self.canvas.bind("<Button-3>", self.finalizar_poligono)
+        self.canvas.bind("<Button-1>", self.controller.iniciar_figura)
+        self.canvas.bind("<B1-Motion>", self.controller.atualizar_figura)
+        self.canvas.bind("<ButtonRelease-1>", self.controller.finalizar_figura)
+        self.canvas.bind("<Button-3>", self.controller.finalizar_poligono)
+
+    def desenhar(self, modelo):
+        self.canvas.delete("all")
+
+        for figura in modelo.figuras:
+            figura.desenhar(self.canvas)
+
+        if modelo.figura_nova:
+            modelo.figura_nova.desenhar_preview(self.canvas)
